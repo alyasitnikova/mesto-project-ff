676275bfc62abe289ca8b24e9d0fee666f14ad8f
Alina Sitnikova <alyasitnikov@yandex.ru>
2024-10-31 23:35:34 +0300

    add js

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.getElementById('card-template').content;
+
+const createCardElement = function (card) {
+    const cardElement = cardTemplate.cloneNode(true);
+  
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').src = card.link;
+
+    return cardElement;
+}
+
+const deleteCard = function (event) {
+    event.target.parentElement.remove();
+}
+
+initialCards.forEach(function (card) {
+    const cardElement = createCardElement(card);
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  
+    placesList.append(cardElement);
+  })
