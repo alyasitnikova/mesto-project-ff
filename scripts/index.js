9153be338a70b04b31071e9d38b17af2a18c2e31
Alina Sitnikova <alyasitnikov@yandex.ru>
2024-11-01 20:33:57 +0300

    fix js

--- scripts/index.js
+++ scripts/index.js
@@ -11,23 +11,24 @@
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.getElementById('card-template').content;
 
-const createCardElement = function (card) {
-    const cardElement = cardTemplate.cloneNode(true);
+const createCardElement = function (card , deleteCard) {
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   
     cardElement.querySelector('.card__title').textContent = card.name;
     cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = `${card.name}: фото местности`;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));
 
     return cardElement;
 }
 
-const deleteCard = function (event) {
-    event.target.parentElement.remove();
+const deleteCard = function (element) {
+    element.remove();
 }
 
 initialCards.forEach(function (card) {
-    const cardElement = createCardElement(card);
-
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    const cardElement = createCardElement(card, deleteCard);
   
     placesList.append(cardElement);
   })
